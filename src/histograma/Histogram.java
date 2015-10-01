2241a7b3cccdf419a093f6b34d3411503443f66f
WonkaD <WonkaD@10.230.169.189>
2015-10-01 10:17:08 +0100

    Histograma v0.2 pero no funciona

---
+++ src/histograma/Histogram.java
@@ -0,0 +1,21 @@
+package histograma;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Histogram<T> {
+    private final HashMap <T,Integer> map = new HashMap(); 
+    
+    public Integer get (T key){
+        return map.get(key);
+    }
+    
+    public Integer increment(T key) {
+        return map.put(key, map.containsKey(key)? map.replace(key, map.get(key)+1) : 1);
+    }
+    
+    public Set<T> keySet(){
+        return map.keySet();
+    }
+    
+}
